a039e2a3bfeee8fc8a555acfa7ae90f950c814f0
Vasily Shorin <86452814+vasshorin@users.noreply.github.com>
2023-01-12 14:42:04 -0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#chatbot-form').submit(function(event) {
+        event.preventDefault();
+        const message = $('#chatbot-input').val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/chatbot',
+            data: {
+                message: message
+            },
+            success: function (response) {
+                let newMessage = $('<div>', { class: 'message' }).text(response.text);
+                let removeButton = $('<button>', { class: 'remove-button' }).text('Remove');
+                newMessage.append(removeButton);
+                $('#chat-history').append(newMessage);
+              }
+        });
+    });
+    $(document).on('click', '.remove-button', function () {
+      $(this).parent().remove();
+    });
+
+  });
+
